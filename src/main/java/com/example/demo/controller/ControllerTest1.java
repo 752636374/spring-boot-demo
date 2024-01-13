0f32d75485d56ea23e459f67a1180e157def2699
yaoheng5 <yaoheng5@jd.com>
2024-01-13 11:45:56 +0800

    测试配置服务

--- src/main/java/com/example/demo/controller/ControllerTest1.java
+++ src/main/java/com/example/demo/controller/ControllerTest1.java
@@ -1,13 +1,21 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerTest1 {
+    @Value("${name}")
+    private String name;
 
     @RequestMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @RequestMapping("/name")
+    public String name() {
+        return name;
     }
 }
